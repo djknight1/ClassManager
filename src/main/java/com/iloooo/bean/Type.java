aed6ed10074d52ba1103a6c0c26c51ad3482e758
lo <ilo114@163.com>
2018-04-20 20:05:50 +0800

    luo

--- src/main/java/com/iloooo/bean/Type.java
+++ src/main/java/com/iloooo/bean/Type.java
@@ -1,4 +1,30 @@
 package com.iloooo.bean;
 
 public class Type {
+    private long id;
+    private String subject;
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "id=" + id +
+                ", subject='" + subject + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
