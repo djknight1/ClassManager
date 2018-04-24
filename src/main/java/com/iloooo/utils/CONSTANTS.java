c3bbbde8a12339e070ee421ddeb52be7f9d9ac64
ilo114 <ilo114@163.com>
2018-04-24 23:37:25 +0800

    luo

--- src/main/java/com/iloooo/utils/CONSTANTS.java
+++ src/main/java/com/iloooo/utils/CONSTANTS.java
@@ -1,7 +1,7 @@
 package com.iloooo.utils;
 
 
-import org.json.JSONObject;
+
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
@@ -9,5 +9,4 @@ import java.io.IOException;
 //  /home/feng/wepapp/root/WEB-INF/file/   taskPath/filename .doc(suffix)
 public  class CONSTANTS {
     public static String FILE_PATH_PREFIX = "WEB-INF/file/";
-    JSONObject jsonObject;
 }
