daed7a4173878515ee3484b78f68d071095c9ad2
Lin1026 <1208781013@qq.com>
2018-04-24 23:20:58 +0800

    lin

--- src/main/java/com/iloooo/utils/CONSTANTS.java
+++ src/main/java/com/iloooo/utils/CONSTANTS.java
@@ -1,5 +1,6 @@
 package com.iloooo.utils;
 
+
 import org.json.JSONObject;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
@@ -8,4 +9,5 @@ import java.io.IOException;
 //  /home/feng/wepapp/root/WEB-INF/file/   taskPath/filename .doc(suffix)
 public  class CONSTANTS {
     public static String FILE_PATH_PREFIX = "WEB-INF/file/";
+    JSONObject jsonObject;
 }
